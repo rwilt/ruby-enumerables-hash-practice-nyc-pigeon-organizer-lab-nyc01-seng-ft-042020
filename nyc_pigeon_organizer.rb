1c29f30095eb9b4baa250acb5e979c1e0144c5cf
rosie w <rosie.wilt@gmail.com>
2020-04-14 19:44:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+
+def nyc_pigeon_organizer (data)
+ data_by_name = {}
+# loop through the hash. get the outer keys and the outer values. the outer value is another hash.
+ data.each do |color_gender_lives,cgl_values|
+ #loop through that inner hash and get the keys (colors, genders, where it lives) and the value, which is the array of names
+  cgl_values.each do |innerkeys ,names_arr|
+#loop through the array of names
+names_arr.each do |name|
+
+#if the name doesnt exist in the new array, add it in and assign it to empty hash
+if data_by_name[name] == nil
+data_by_name[name] = {}
+end
+
+#if the category doesnt exist in the new hash, add it in and set it equal to an empty array
+if data_by_name[name][color_gender_lives] == nil
+data_by_name[name][color_gender_lives] = []
+
+end
+data_by_name[name][color_gender_lives].push(innerkeys.to_s)
+  end
+
+ end
+end
+data_by_name
 end
